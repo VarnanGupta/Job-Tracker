993f64e61a12cfec1b1c9b3ae69ab9ebe54bf3ea
VarnanGupta <varnangupta112@gmail.com>
2024-09-09 15:24:47 +0530

    logout functionality done

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 
 import { NavLink } from "react-router-dom";
 import { Button } from "antd";
+import { get } from "../../services/ApiEndPoint.js";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate()
+  const handleLogout=async()=>{
+    try {
+      const request = await get('/auth/logout')
+      const response = request.data
+      console.log(response)
+      if(response.success){
+        toast.success(response.message)
+        navigate('/login')
+      } 
+    } catch (error) {
+      console.log(error)
+    }
+  }
   
   return (
     <div className="md:h-full md:w-[250px] text-black bg-[#6D8A96] shadow-2xl shadow-slate-950 border-b-2 border-black flex md:flex-col justify-between">
@@ -16,10 +33,11 @@ function Navbar() {
         <NavLink to={""}>Build Resume</NavLink>
       </nav>
       <nav className="flex md:flex-col gap-3 text-2xl font-semibold font-mono ml-5 mb-10">
-        <NavLink to={"/login"}>Login</NavLink>
-        <NavLink to={"/register"}>Register</NavLink>
+        {/* <NavLink to={"/login"}>Login</NavLink>
+        <NavLink to={"/register"}>Register</NavLink> */}
         <Button
         type="primary"
+        onClick={handleLogout}
         className=" bg-black text-white font-mono flex justify-center rounded-3xl p-3 px-4 mt-2 mx-11"
         >
         Logout
