aeb372e18d8895cc43e65d0a0a9faa6ee28ab3a1
VarnanGupta <varnangupta112@gmail.com>
2024-09-09 20:54:55 +0530

    Navbar edit

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import { NavLink } from "react-router-dom";
 import { Button } from "antd";
 import { get } from "../../services/ApiEndPoint.js";
@@ -7,21 +6,21 @@ import { useNavigate } from "react-router-dom";
 import { BiLinkExternal } from "react-icons/bi";
 
 function Navbar() {
-  const navigate = useNavigate()
-  const handleLogout=async()=>{
+  const navigate = useNavigate();
+  const handleLogout = async () => {
     try {
-      const request = await get('/auth/logout')
-      const response = request.data
-      console.log(response)
-      if(response.success){
-        toast.success(response.message)
-        navigate('/login')
-      } 
+      const request = await get("/auth/logout");
+      const response = request.data;
+      console.log(response);
+      if (response.success) {
+        toast.success(response.message);
+        navigate("/login");
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-  
+  };
+
   return (
     <div className="md:h-full md:w-[250px] z-10 overflow-y-auto text-white bg-[#141411] shadow-2xl shadow-black border-b-2 border-black flex md:flex-col justify-between">
       <p className="text-4xl font-bold mt-[40px] ml-10">Job Tracker</p>
@@ -31,21 +30,25 @@ function Navbar() {
         <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/tracker"}>Tracker</NavLink>
         {/* <NavLink to={""}>Build Resume</NavLink> */}
-        <a className="flex items-center gap-2" href="https://pro-khar.github.io/resume-builder/" target="_blank">Build Resume <BiLinkExternal /> </a>
+        <a
+          className="flex items-center gap-2"
+          href="https://pro-khar.github.io/resume-builder/"
+          target="_blank"
+        >
+          Build Resume <BiLinkExternal />{" "}
+        </a>
       </nav>
       <nav className="flex md:flex-col gap-3 text-2xl font-semibold font-mono ml-5 mb-10">
         {/* <NavLink to={"/login"}>Login</NavLink>
         <NavLink to={"/register"}>Register</NavLink> */}
         <Button
-        type="primary"
-        onClick={handleLogout}
-        className=" bg-red-500 text-white font-mono flex justify-center rounded-3xl p-3 px-4 mt-2 mx-11"
+          type="primary"
+          onClick={handleLogout}
+          className=" bg-red-500 text-white font-mono flex justify-center rounded-3xl p-3 px-4 mt-2 mx-11"
         >
-        Logout
-      </Button>
-
+          Logout
+        </Button>
       </nav>
-      
     </div>
   );
 }
