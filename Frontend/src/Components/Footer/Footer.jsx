68a149663d738f5cd9287faa9f97001a7d45eda8
VarnanGupta <varnangupta112@gmail.com>
2024-09-09 19:26:09 +0530

    A lot of work done in frontend styling

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      {/* FOOTER */}
+    <footer className="bg-gray-400 text-yellow-400 py-8 font-mono">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Job Application Tracker. All rights reserved. Made by <span>Varnan Gupta</span> </p>
+      </div>
+    </footer>
+    </div>
   )
 }
 
--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
@@ -7,6 +7,7 @@ function Layout() {
     <div className="flex flex-col md:flex-row h-screen">
       <Navbar />
       <Outlet />
+      {/* <Footer /> */}
     </div>
   );
 }
